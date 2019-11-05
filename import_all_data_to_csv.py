4826686564853e1c3b118fdf755215d2a033cdd6
Gagandeep Singh <gagan@gagan.local>
2019-11-04 21:59:52 -0500

    imports data from firestore and creates csv file. detected_languages.csv and errors.csv are 2 csv files created.

--- import_all_data_to_csv.py
+++ import_all_data_to_csv.py
@@ -1,27 +1,45 @@
+# This file will import all data from firebase and convert to different csv
+#
+
+
 from google.cloud import firestore
+from langdetect import detect
+from langdetect import DetectorFactory
+from langdetect.lang_detect_exception import  LangDetectException
+
+DetectorFactory.seed = 0
+
 db = firestore.Client("poetry-dev")
 items_ref = db.collection(u'items')
 items = items_ref.get()
-poem_no = 0
-language_set = 0
-language_not_set = 0
+
+result_csv_file = open("detected_languages.csv", "w")
+error_csv_file = open("errors.csv", "w")
+result_csv_file.write("")
 
 for item in items:
+    poem_body = item.get("body")
+    body = poem_body
+    # print(item.get('body'))
+    # print(body[0:100])
 
-    poem_num = poem_num + 1
-    item_language = item.get("language")
-    if (item_language == "unknown_XX") or (item_language == "not supported"):
-        language_not_set = language_not_set + 1
-    else :
-        language_set = language_set + 1
-
-print(str(language_not_set) + ' poems do not have language set.')
-print(str(language_set) + ' poems have language set.')
-print(str(poem_num) + " poems are there in total")
-
-print(str((language_not_set * 1.0) / poem_num * 100) + "% poems don't have language set.")
-
-
-
+    try:
+      detected_lang = detect(body)
+      result_csv_file.write(item.get("itemId"))
+      result_csv_file.write(', ')
+      result_csv_file.write(detected_lang)
+      result_csv_file.write(', ')
+      result_csv_file.write(item.get("language"))
+      # result_csv_file.write(item.get('body'))
+      result_csv_file.write("\n ")
 
 
+    except LangDetectException as e:
+
+      error_csv_file.write(body)
+      error_csv_file.write(", ")
+      # error_csv_file.write("LangDetectException" + e.message)
+      error_csv_file.write("\n")
+
+    except Exception as e:
+      print(e)
